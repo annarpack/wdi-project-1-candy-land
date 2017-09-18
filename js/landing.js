1ace4675683f84819a7485fe21f448a8b2370d83
Anna R Pack <annarpack@gmail.com>
2017-09-18 15:18:14 -0400

    game finished

--- js/landing.js
+++ js/landing.js
@@ -1,6 +1,18 @@
 $('document').ready(function(){
 
 
+function checkValue() {
+	$('#submit-button').click(function(){
+		var players = window.localStorage.getItem('playersNum');
+		if (players === '') {
+			alert('Please choose a number between 1 and 4.')
+		} 
+
+	})
+}
+
+
+
 function setPlayers() {
 
 	var players = window.localStorage.getItem('playersNum');
